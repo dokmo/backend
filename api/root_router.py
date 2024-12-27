b521a40d8d716dc379a3be2b1f0ac0903ebd44ce
TrulyNotMalware <notleebutyee@gmail.com>
2024-12-27 16:19:21 +0900

    test : example router

--- api/root_router.py
+++ api/root_router.py
@@ -1,3 +1,5 @@
 from fastapi import APIRouter
+from api.v1.api_router import api_router
 
 root_router = APIRouter()
+root_router.include_router(api_router, prefix="/api")
---
+++ api/v1/api_router.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+from api.v1.example.example import example_router
+
+api_router = APIRouter()
+api_router.include_router(example_router, prefix="/example")
