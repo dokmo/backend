9314ef53de6bcbbb510de6822eb0ea01f47956ba
TrulyNotMalware <notleebutyee@gmail.com>
2025-01-03 17:19:07 +0900

    append test methods

--- api/v1/example/example.py
+++ api/v1/example/example.py
@@ -9,3 +9,11 @@ async def hello_world():
 @example_router.post(path="")
 async def hello_post(data: dict):
     return data
+
+@example_router.delete(path="")
+async def hello_delete():
+    return "delete"
+
+@example_router.patch(path="")
+async def hello_patch(data:dict):
+    return data
