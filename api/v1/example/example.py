1ec5148007e5be63e8e6111b317aa0b8cdcb78ba
TrulyNotMalware <notleebutyee@gmail.com>
2025-01-03 17:03:00 +0900

    test post requests

--- api/v1/example/example.py
+++ api/v1/example/example.py
@@ -5,3 +5,7 @@ example_router = APIRouter()
 @example_router.get(path="")
 async def hello_world():
     return "hello_world!"
+
+@example_router.post(path="")
+async def hello_post(data: dict):
+    return data
