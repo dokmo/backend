392e48cf5a710cb33a9655d2a0e3a24f5a89734c
Kim <zxcklwe@naver.com>
2025-01-17 18:12:02 +0900

    증복된 user model 데이터 테이블 제거

--- app/user/infrastructure/model/user.py
+++ app/user/infrastructure/model/user.py
@@ -7,7 +7,6 @@ from uuid import UUID
 
 class UserModel(Base, TimeStamp):
     __tablename__ = "user"
-
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     kakao_id: Mapped[str] = mapped_column(String, nullable=False)
     user_id: Mapped[UUID] = mapped_column(unique=True)
--- app/user/service/service.py
+++ app/user/service/service.py
@@ -1,34 +1,32 @@
-from app.user.model.User import User
+from app.user.infrastructure.model.user import UserModel
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.orm import selectinload
 
 class UserService:
     def __init__(self, session: AsyncSession):
-        self.session = session
-
+        self.__meet_repository = session #FIXME
     async def get_user(self, kakao_user_id: str):
-        async with self.session() as session:
-            result = await session.execute(select(User).filter(User.kakao_user_id == kakao_user_id))
+        async with self.__meet_repository() as session:
+            result = await session.execute(select(UserModel).filter(UserModel.kakao_user_id == kakao_user_id))
             user = result.scalars().first()  # 첫 번째 사용자만 반환
             return user
 
     async def create_user(self, user_id: str, nickname: str, kakao_user_id: str):
-        async with self.session() as session:
-            user = User(user_id=user_id, nickname=nickname, kakao_user_id=kakao_user_id)
+        async with self.__meet_repository() as session:
+            user = UserModel(user_id=user_id, nickname=nickname, kakao_user_id=kakao_user_id)
             session.add(user)
             await session.commit()
             return user
 
     async def get_all_users(self):
-        async with self.session() as session:
-            result = await session.execute(select(User))
+        async with self.__meet_repository() as session:
+            result = await session.execute(select(UserModel))
             users = result.scalars().all()
             return users
 
     async def update_user(self, user_id: str, nickname: str):
-        async with self.session() as session:
-            user = await session.execute(select(User).filter(User.user_id == user_id))
+        async with self.__meet_repository() as session:
+            user = await session.execute(select(UserModel).filter(UserModel.user_id == user_id))
             user = user.scalars().first()
             if user:
                 user.nickname = nickname
